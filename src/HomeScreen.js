193364d0c9999aa09a34ca25c1d432e9d7805544
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2021-05-10 17:23:57 +0700

    Nam committ

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -131,10 +131,10 @@ const HomeScreen =()=>{
       <MapView provider={PROVIDER_GOOGLE}
       style={styles.map}
       initialRegion={{
-        latitude: 37.78825,
-         longitude: -122.4324,
+        latitude: 37.679453,
+         longitude: 139.6887197,
          latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421,
+         longitudeDelta: 0.0922,
       }}
       
       >
