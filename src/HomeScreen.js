ab63611fca65da97553824b096a91ef6fa2411a5
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2021-05-12 23:56:03 +0700

    Nam committ

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -128,28 +128,29 @@ import { View,StyleSheet } from "react-native";
 const HomeScreen =()=>{
   return(
     <View style={styles.container}>
-      <MapView provider={PROVIDER_GOOGLE}
-      style={styles.map}
-      initialRegion={{
-        latitude: 37.679453,
-         longitude: 139.6887197,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0922,
-      }}
-      
-      >
-
-      </MapView>
-    </View>
+     <MapView
+       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+       style={styles.map}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+   </View>
   )
 }
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'green'
+  container: {
+    ...StyleSheet.absoluteFillObject,
+  flex:1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
-  map:{
-    flex:1
-  }
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
 })
 export default HomeScreen
